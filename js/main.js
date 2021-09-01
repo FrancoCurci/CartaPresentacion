a6d5c9ad40e88fa51c52fa08a031491a0c6228ea
Franco <francocurci@hotmail.com>
2021-09-01 15:03:42 -0300

    faltan las animaciones js

--- js/main.js
+++ js/main.js
@@ -7,9 +7,8 @@ console.log(tecnologiasClass);
 for (var i = 0; i < tecnologiasClass.length; i++) {
     console.log(tecnologiasClass[i]);
     if (i !== 0) {
-        tecnologiasClass[i].setAttribute("hidden", "true");
+        tecnologiasClass[i].classList.add("ocultar-seccion");
     }
-
     console.log("entra")
 }
 
